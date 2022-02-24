834f44c3b216967f9a0c470fb3f213f8b7cb96dc
kongukjae <kongukjae@gmail.com>
2022-02-25 02:21:44 +0900

    update

--- email.js
+++ email.js
@@ -3,18 +3,18 @@ const studentList = [];
 studentList.push("강사 공욱재-----------------------");
 studentList.push("권수연 balenti808@gmail.com");
 studentList.push("김주은(11월) jrangkim11@gmail.com");
-studentList.push("김주은(12월) jjj4779@naver.com"); // * check
-studentList.push("김준식 wkxndlsvk@naver.com"); // * check
-studentList.push("김지현 kwlgus0396@gmail.com");
-studentList.push("손만충 asd552288@naver.com"); // * check
+studentList.push("김주은(12월) jueun4779@gmail.com"); // * check
+studentList.push("김준식 wkxndlsvk@gmail.com"); // * check
+studentList.push("김지현 kwlgus0318@gmail.com");
+studentList.push("손만충 sunwanchung@gmail.com"); // * check
 studentList.push("송주현 joohyun0403@gmail.com");
-studentList.push("오요섭 dhdytjq2@naver.com"); // * check
-studentList.push("이누리 depp1031@naver.com"); // * check
-studentList.push("이성윤 queen1448@nate.com"); // * check
+studentList.push("오요섭 dhdytjq2@gmail.com"); // * check
+studentList.push("이누리 leenuri1031@gmail.com"); // * check
+studentList.push("이성윤 mementomorii666@gmail.com"); // * check
 studentList.push("이아진 a.jinlee92@gmail.com");
 studentList.push("이지은 liz950827@gmail.com");
 studentList.push("이채연 dlcodus124@naver.com"); // * check
-studentList.push("임성민 interkq@naver.com"); // * check
+studentList.push("임성민 interkp12@gmail.com"); // * check
 studentList.push("장우진 woojin961215@gmail.com");
 studentList.push("장준형 wkdwnsgud1@gmail.com");
 studentList.push("정수임 spdlqjtn159@gmail.com");
