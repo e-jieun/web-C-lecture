2625a3269669c96820d329d9eda72308565bdc40
kongukjae <kongukjae@gmail.com>
2022-05-23 16:58:59 +0900

    20220523 프로젝트 디렉토리 구조설계론

---
+++ [10]code-pattern/directory-setting/js/module/b.js
@@ -0,0 +1,9 @@
+const mainObject = {
+  name : "export defalut example",
+  fileName : "b.js",
+  function : "export, import",
+  explain : "자바스크립트는 내보내기 가져오기 기능을 지원합니다."
+}
+
+
+export default mainObject
