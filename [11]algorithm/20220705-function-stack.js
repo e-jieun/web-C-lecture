ae61d806dfb11dda14d114e5701a6f58302dfc9a
kongukjae <kongukjae@gmail.com>
2022-07-05 17:48:42 +0900

    20220705 함수 동작 연습 예제

---
+++ [11]algorithm/20220705-function-stack.js
@@ -0,0 +1,16 @@
+function one() {
+  console.log('this is one function');
+  two();
+}
+function two() {
+  console.log('this is two function');
+  three();
+}
+function three() {
+  console.log('this is three function');
+  four();
+}
+function four() {
+  console.log('this is four function');
+}
+one();
